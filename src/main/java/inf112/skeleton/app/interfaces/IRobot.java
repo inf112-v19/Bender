6ef9b821d6111320ab2ba0df2c0d8ce7a51cf287
Rune Thelen <46781497+RuneThe@users.noreply.github.com>
2019-01-30 09:48:35 +0100

    minor corrections

--- src/main/java/inf112/skeleton/app/interfaces/IRobot.java
+++ src/main/java/inf112/skeleton/app/interfaces/IRobot.java
@@ -2,9 +2,9 @@ package inf112.skeleton.app.interfaces;
 // RT initial setup jan 29/30 - 2019
 public interface IRobot {
 
-    public void move();         // int = -1,0,1,2,3 (steps)  char = N,S,E,W (direction) or int = 0, 90, 180, 270
+    public void move();         // int = -1,1,2,3 (steps)
     public void turn();         // int = 90, 270, 180 degrees
-    public void getDirection(); // get current direction of robot as char (N,S,E,W) or int (0, 90, 280, 270)
+    public void getDirection(); // get current direction of robot as int (0, 90, 280, 270)
     public void giveCards();    // give chosen object(with card ID) of 5 cards to robot, to put in RobotMemory
   
     public int takeEnergy();    // receive damage and update energy, return new energy
