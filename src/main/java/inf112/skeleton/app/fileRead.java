d7d98f7de6ca1f60dbc900c48a175357d43bd673
Rune Thelen <46781497+RuneThe@users.noreply.github.com>
2019-02-01 08:28:28 +0100

    updates

--- src/main/java/inf112/skeleton/app/fileRead.java
+++ src/main/java/inf112/skeleton/app/fileRead.java
@@ -2,20 +2,22 @@
 // ** Created 1.feb 2019 Rune Thelen
 
 package inf112.skeleton.app;
-package io;
 import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class fileRead {
+    public static void main(String[] args) throws IOException {
+        String fileName = "cardData.txt.txt";
+        ArrayList<String> linesInFile = new ArrayList<String>();
+        FileReader fileReader = new FileReader(fileName);
 
-    FileInputStream fileStream = new FileInputStream("cardData.txt");
-    BufferedReader br = new BufferedReader(new InputStreamReader(fileStream));
-
-    String strLine;
-    ArrayList<String> linesInFile = new ArrayList<String>();
-
-    while ((strLine = br.readLine()) != null)   {
-        linesInFile.add(strLine);
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                linesInFile.add(line);
+            }
+        }
     }
-fileStream.close();
 }
