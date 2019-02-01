4e368221553fdb0a9e68597a118cec90c385d56c
Rune Thelen <46781497+RuneThe@users.noreply.github.com>
2019-02-01 09:53:48 +0100

    edit reference of file in repositry

--- src/main/java/inf112/skeleton/app/fileRead.java
+++ src/main/java/inf112/skeleton/app/fileRead.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class fileRead {
     public static void main(String[] args) throws IOException {
-        String fileName = "src/main/java/inf112/skeleton/app/cardData.txt";
+        String fileName = "src/main/java/inf112/skeleton/app/cards/cardData.txt";
         ArrayList<String> linesInFile = new ArrayList<String>();
         FileReader fileReader = new FileReader(fileName);
 
