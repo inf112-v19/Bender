9f51554af741685ed0e59cac77943d8fe7264e5c
Petter Daae <petter.daae@gmail.com>
2019-02-02 13:10:33 +0100

    add unique priorities to the standard program deck

--- src/main/java/inf112/skeleton/app/cards/ProgramDeck.java
+++ src/main/java/inf112/skeleton/app/cards/ProgramDeck.java
@@ -12,20 +12,15 @@ public class ProgramDeck extends Deck<IProgramCard> {
     private static Iterable<IProgramCard> standardProgramDeck() {
         List<IProgramCard> cards = new ArrayList<>();
 
-        for (int i = 0; i < 18; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.FORWARDS, 1));
-        for (int i = 0; i < 12; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.FORWARDS, 2));
-        for (int i = 0; i < 6; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.FORWARDS, 3));
-        for (int i = 0; i < 6; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.BACKWARDS, 1));
+        for (int i = 0; i < 18; i++) cards.add(new MoveCard(490 + (i * 10), MoveCard.Direction.FORWARDS, 1));
+        for (int i = 0; i < 12; i++) cards.add(new MoveCard(670 + (i * 10), MoveCard.Direction.FORWARDS, 2));
+        for (int i = 0; i < 6; i++) cards.add(new MoveCard(790 + (i * 10), MoveCard.Direction.FORWARDS, 3));
+        for (int i = 0; i < 6; i++) cards.add(new MoveCard(430 + (i * 10), MoveCard.Direction.BACKWARDS, 1));
 
-        for (int i = 0; i < 18; i++) cards.add(new RotateCard(priority(), RotateCard.Direction.RIGHT));
-        for (int i = 0; i < 18; i++) cards.add(new RotateCard(priority(), RotateCard.Direction.LEFT));
-        for (int i = 0; i < 6; i++) cards.add(new RotateCard(priority(), RotateCard.Direction.UTURN));
+        for (int i = 0; i < 18; i++) cards.add(new RotateCard(80 + (i * 20), RotateCard.Direction.RIGHT));
+        for (int i = 0; i < 18; i++) cards.add(new RotateCard(70 + (i * 20), RotateCard.Direction.LEFT));
+        for (int i = 0; i < 6; i++) cards.add(new RotateCard(10 + (i * 10), RotateCard.Direction.UTURN));
 
         return cards;
-    }
-
-    private static int priority() {
-        // TODO: implement
-        return 0;
     }
 }
