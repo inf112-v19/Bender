9782f1a73ba92432d1f04273e47b2be60c5bec9b
Rune Thelen <46781497+RuneThe@users.noreply.github.com>
2019-02-03 08:33:23 +0100

    Set new order of cards and made priority()
    Changed order of making cards, to reflect setup in list, according to prioriry. 
    Priority() now trivial, and alternation of Left/right rotation is now grouped in same loop, to gain correct mix of priority.

--- src/main/java/inf112/skeleton/app/cards/ProgramDeck.java
+++ src/main/java/inf112/skeleton/app/cards/ProgramDeck.java
@@ -11,21 +11,24 @@ public class ProgramDeck extends Deck<IProgramCard> {
 
     private static Iterable<IProgramCard> standardProgramDeck() {
         List<IProgramCard> cards = new ArrayList<>();
-
+        
+        for (int i = 0; i < 6; i++) cards.add(new RotateCard(priority(), RotateCard.Direction.UTURN));       
+        
+        for (int i = 0; i < 36; i++) { 
+            cards.add(new RotateCard(priority(), RotateCard.Direction.RIGHT));
+            cards.add(new RotateCard(priority(), RotateCard.Direction.LEFT));
+        }
+        
         for (int i = 0; i < 18; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.FORWARDS, 1));
         for (int i = 0; i < 12; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.FORWARDS, 2));
         for (int i = 0; i < 6; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.FORWARDS, 3));
         for (int i = 0; i < 6; i++) cards.add(new MoveCard(priority(), MoveCard.Direction.BACKWARDS, 1));
-
-        for (int i = 0; i < 18; i++) cards.add(new RotateCard(priority(), RotateCard.Direction.RIGHT));
-        for (int i = 0; i < 18; i++) cards.add(new RotateCard(priority(), RotateCard.Direction.LEFT));
-        for (int i = 0; i < 6; i++) cards.add(new RotateCard(priority(), RotateCard.Direction.UTURN));
-
+        
         return cards;
     }
 
     private static int priority() {
-        // TODO: implement
-        return 0;
+        for (j = 10; j <= 840; j += 10
+        return j;
     }
 }
