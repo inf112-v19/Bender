b5c0469d8cf3ac49c2ed0f3357d0635d37acf714
andreas <andreas.atakan@student.uib.no>
2019-02-05 11:46:00 +0100

    Added methods to ITile and IBoard interfaces

--- src/main/java/inf112/skeleton/app/interfaces/ITile.java
+++ src/main/java/inf112/skeleton/app/interfaces/ITile.java
@@ -1,4 +1,18 @@
 package inf112.skeleton.app.interfaces;
 
+import inf112.skeleton.app.enums.TileType;
+import inf112.skeleton.app.enums.TileColor;
+import inf112.skeleton.app.enums.Dir;
+
 public interface ITile {
+    TileType getType();             // Method for getting the tile type
+    void setType(TileType tile);    // Method for setting the tile type
+
+    ITile getNeighbor(Dir dir);     // Method for getting the nearest neighbor
+
+    IPlayer getPlayer();            // Method for getting the current player on the tile
+    void setPlayer(IPlayer player); // Method for setting the current player on the tile
+
+    TileColor getColor();           // Method for getting the tile color
+    void setColor(TileColor color); // Method for setting the tile color
 }
