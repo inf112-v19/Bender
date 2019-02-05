626b91a3d2d2f66b621e1db3fb36b6666bbc9e28
Rune Thelen <rth002@uib.no>
2019-02-05 14:04:51 +0100

    small corrections

--- src/main/java/inf112/skeleton/app/fileRead.java
+++ src/main/java/inf112/skeleton/app/fileRead.java
@@ -1,15 +1,22 @@
+// ** PROJECT BENDER / ROBOrally 2019
+// ** Created 1.feb 2019 Rune Thelen
+
 package inf112.skeleton.app;
-package io;
 import java.io.*;
+import java.util.ArrayList;
 
 public class fileRead {
+    public static void main(String[] args) throws IOException {
+        String fileName = "src/main/java/inf112/skeleton/app/cardData.txt";
+        ArrayList<String> linesInFile = new ArrayList<String>();
+        FileReader fileReader = new FileReader(fileName);
 
-    public static void main(String[] args) throws Exception {
-            FileReader fr = new FileReader("C:\\Users\\pankaj\\Desktop\\test.java");
-            int i;
-            while ((i=fr.read()) != -1){
-                System.out.print((char) i);
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                linesInFile.add(line);
+                System.out.println(line);       // only for test purpose, REMOVE in production
             }
+        }
     }
-
 }
