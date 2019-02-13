37cc97ed7b0a77239f2c44192ff6d50b208a3ec5
RuneThe <rth002@uib.no>
2019-02-13 20:45:16 +0100

    robot first version

--- src/main/java/inf112/skeleton/app/interfaces/IRobot.java
+++ src/main/java/inf112/skeleton/app/interfaces/IRobot.java
@@ -1,17 +1,19 @@
 package inf112.skeleton.app.interfaces;
+
+import inf112.skeleton.app.enums.Direction;
+
 // RT initial setup jan 29/30 - 2019 - cont feb 12
 public interface IRobot {
 
-    public void getDirection(); // give current direction of robot as ENUM dir();
-    public void setDirection(); // set direction after action from board (turn from card or tile)
-    public void giveCards();    // give chosen object(with card ID) of 5 cards to robot, to put in RobotMemory
+    public Direction getDirection(); // give current direction of robot as ENUM dir();
+    public void setDirection(Direction); // set direction after action from board (turn from card or tile)
 
-    public int takeEnergy();    // receive damage (int) and update energy, return new energy (int)
-    public int giveEnergy();    // boost energy (int) from action or card, return new energy (int)
+    void setDirection(Direction setDirection);
+
+    public int takeEnergy(int energy);    // receive damage (int) and update energy, return new energy (int)
+    public int giveEnergy(int energy);    // boost energy (int) from action or card, return new energy (int)
     public int checkEnergy();   // return energy level (int) of current robot
 
-    public void shootLazer();   // int = force -> fire lazer in current direction (from getDirection();)
-
-    public void powerDown();    // power down for backup
+    public void shootLazer();   // fire lazer in current direction (from getDirection();)
 
 }
