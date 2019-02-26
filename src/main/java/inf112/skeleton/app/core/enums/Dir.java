00b12b76baa7d6295d10e481bb34786649ce2f38
andreas <andreas.atakan@student.uib.no>
2019-02-26 12:58:44 +0100

    Created tile tests

--- src/main/java/inf112/skeleton/app/core/enums/Dir.java
+++
@@ -1,5 +0,0 @@
-package inf112.skeleton.app.core.enums;
-
-public enum Dir {
-    NORTH, SOUTH, EAST, WEST, NONE
-}
---
+++ src/test/java/inf112/skeleton/app/core/tiles/TileGearTest.java
@@ -0,0 +1,4 @@
+package inf112.skeleton.app.core.tiles;
+
+public class TileGearTest {
+}
