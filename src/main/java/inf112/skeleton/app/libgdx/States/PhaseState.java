20825fb4f3bb0e28c02fc7f78794c5c22d58088d
Dovydas Sprindys <spdoviss@gmail.com>
2019-03-01 22:07:29 +0100

    made the phase state to draw a basic board, which can be used for testing

--- src/main/java/inf112/skeleton/app/libgdx/States/PhaseState.java
+++ src/main/java/inf112/skeleton/app/libgdx/States/PhaseState.java
@@ -1,10 +1,15 @@
 package inf112.skeleton.app.libgdx.States;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import inf112.skeleton.app.libgdx.VisualBoardLoader;
+
+import java.io.IOException;
 
 public class PhaseState extends State{
-    protected PhaseState(GameStateManager gsm) {
+    private VisualBoardLoader visualBoardLoader;
+    protected PhaseState(GameStateManager gsm) throws IOException {
         super(gsm);
+        visualBoardLoader = new VisualBoardLoader("res/boards/sampleboard1.txt");
     }
 
     @Override
@@ -19,6 +24,9 @@ public class PhaseState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.begin();
+        visualBoardLoader.renderBoard(sb);
+        sb.end();
 
     }
 
