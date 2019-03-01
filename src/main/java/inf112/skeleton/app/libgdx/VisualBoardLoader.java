98c5ac9a5b291da3d959b569bc06701f2f6066ad
Dovydas Sprindys <spdoviss@gmail.com>
2019-03-01 22:05:53 +0100

    Added a basic graphical loader of the board

---
+++ src/main/java/inf112/skeleton/app/libgdx/VisualBoardLoader.java
@@ -0,0 +1,68 @@
+package inf112.skeleton.app.libgdx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import inf112.skeleton.app.core.board.BoardLoader;
+import inf112.skeleton.app.core.tiles.Tile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class VisualBoardLoader {
+    private BoardLoader boardloader;
+    private Map<Integer, String> tilePathNameToNumber;
+    private Texture[] tileTextures;
+    int[][] tiles;
+
+    public VisualBoardLoader(String filepath) throws IOException {
+        tileTextures = new Texture[15];
+        tilePathNameToNumber = new HashMap<>();
+        boardloader = new BoardLoader();
+
+        makeMap();
+        initializeTextures();
+
+        tiles = boardloader.loadFile(filepath);
+    }
+
+    private void initializeTextures() {
+        for (int i = 0; i < tileTextures.length; i++) {
+            System.out.println(i);
+            System.out.println(tilePathNameToNumber.get(i));
+            tileTextures[i] = new Texture(Gdx.files.internal(tilePathNameToNumber.get(i)));
+        }
+    }
+    //The SpriteBatch should be started prior calling this method
+    public void renderBoard(SpriteBatch sb) {
+        for (int y = 0; y < tiles.length; y++) {
+            for (int x = 0; x < tiles[y].length; x++) {
+                sb.draw(tileTextures[tiles[x][y]], x*64, y*64);
+            }
+        }
+
+
+    }
+
+    private void makeMap() {
+        tilePathNameToNumber.put(0, "res/tiles/empty_tile.png");
+        tilePathNameToNumber.put(1, "res/tiles/rotate_left_tile.png");
+        tilePathNameToNumber.put(2, "res/tiles/rotate_right_tile.png");
+        tilePathNameToNumber.put(3, "res/tiles/assembly_orange_straight_down_tile.png");
+        tilePathNameToNumber.put(4, "res/tiles/assembly_orange_straight_up_tile.png");
+        tilePathNameToNumber.put(5, "res/tiles/assembly_orange_straight_right_tile.png");
+        tilePathNameToNumber.put(6, "res/tiles/assembly_orange_straight_left_tile.png");
+
+        tilePathNameToNumber.put(7, "res/tiles/assembly_orange_turn_left_up_tile.png");
+        tilePathNameToNumber.put(8, "res/tiles/assembly_orange_turn_left_down_tile.png");
+        tilePathNameToNumber.put(9, "res/tiles/assembly_orange_turn_right_up_tile.png");
+        tilePathNameToNumber.put(10, "res/tiles/assembly_orange_turn_right_down_tile.png");
+        tilePathNameToNumber.put(11, "res/tiles/assembly_orange_turn_down_right_tile.png");
+        tilePathNameToNumber.put(12, "res/tiles/assembly_orange_turn_down_left_tile.png");
+        tilePathNameToNumber.put(13, "res/tiles/assembly_orange_turn_up_left_tile.png");
+        tilePathNameToNumber.put(14, "res/tiles/assembly_orange_turn_up_right_tile.png");
+    }
+}
+
