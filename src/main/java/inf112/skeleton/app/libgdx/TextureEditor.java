ddb116af83883013de10df73de207296fab990fd
Dovydas Sprindys <spdoviss@gmail.com>
2019-03-22 17:44:44 +0100

    fixed mergeTetures method

--- src/main/java/inf112/skeleton/app/libgdx/TextureEditor.java
+++ src/main/java/inf112/skeleton/app/libgdx/TextureEditor.java
@@ -8,8 +8,9 @@ import com.badlogic.gdx.graphics.TextureData;
 
 public class TextureEditor {
 
-    public Texture rotate90(String path) {
-        Pixmap pixmap = new Pixmap(Gdx.files.internal(path));
+    public Texture rotate90(Texture texture) {
+        TextureData textureData = texture.getTextureData();
+        Pixmap pixmap = textureData.consumePixmap();
         final int height = pixmap.getHeight();
         final int width = pixmap.getWidth();
         Pixmap rotated = new Pixmap(height, width, pixmap.getFormat());
@@ -22,15 +23,17 @@ public class TextureEditor {
         return new Texture(rotated);
     }
 
-    public Texture changeColour(String path, float r, float g, float b, float a) {
-        Pixmap pixmap = new Pixmap(Gdx.files.internal(path));
+    public Texture changeColour(Texture texture, float r, float g, float b, float a) {
+        TextureData textureData = texture.getTextureData();
+        Pixmap pixmap = textureData.consumePixmap();
         pixmap.setColor(r, g, b, a);
         return new Texture(pixmap);
     }
 
     //Iterates through each pixel in a given texture, darkening it by given percentage
-    public Texture darkenTexture(String path, float percentage) {
-        Pixmap pixmap = new Pixmap(Gdx.files.internal(path));
+    public Texture darkenTexture(Texture texture, float percentage) {
+        TextureData textureData = texture.getTextureData();
+        Pixmap pixmap = textureData.consumePixmap();
         Color color = new Color();
         final int height = pixmap.getHeight();
         final int width = pixmap.getWidth();
@@ -56,7 +59,7 @@ public class TextureEditor {
 //                newPixamp.setColor(newR, newG, newB, A-50);
                 newColor.set(newR, newG, newB, A);
                 //ARGB -- > RGBA
-                newPixamp.drawPixel(x,y, newColor.toIntBits());
+                newPixamp.drawPixel(x, y, newColor.toIntBits());
 
             }
 //        System.out.println("R: " + R + " newR: " + newR);
@@ -69,34 +72,42 @@ public class TextureEditor {
 
     //Currently assumes that the textures have same dimensions
     public Texture mergeTextures(Texture frontTexture, Texture backTexture) {
+        frontTexture = this.resizeTexture(backTexture.getWidth(), backTexture.getHeight(), frontTexture);
         TextureData frontTextureData = frontTexture.getTextureData();
         TextureData backTextureData = backTexture.getTextureData();
-
+//        frontTextureData.prepare();
         Pixmap frontPixmap = frontTextureData.consumePixmap();
+
+        backTextureData.prepare();
         Pixmap backPixmap = backTextureData.consumePixmap();
 
         final int textureHeight = frontPixmap.getHeight();
         final int textureWidth = frontPixmap.getWidth();
 
+        Pixmap newPixmap = new Pixmap(textureWidth, textureHeight, frontPixmap.getFormat());
         for (int x = 0; x < textureWidth; x++)
             for (int y = 0; y < textureHeight; y++) {
                 int frontVal = frontPixmap.getPixel(x, y);
-                backPixmap.drawPixel(x, y, frontVal);
+//                System.out.println(frontVal);
+                if (frontVal != 255)
+                    backPixmap.drawPixel(x, y, frontVal);
             }
         return new Texture(backPixmap);
 
     }
 
-    public Texture changeToColourRed(String path) {
-        return changeColour(path, 90f / 255f, 14f / 255f, 14f / 255f, 255f / 255f);
+    public Texture changeToColourRed(Texture texture) {
+        return changeColour(texture, 90f / 255f, 14f / 255f, 14f / 255f, 255f / 255f);
     }
 
-    public Texture colourBlue(String path) {
-        return changeColour(path, 25f / 255f, 25f / 255f, 112f / 255f, 255f / 255f);
+    public Texture colourBlue(Texture texture) {
+        return changeColour(texture, 25f / 255f, 25f / 255f, 112f / 255f, 255f / 255f);
     }
 
-    public Texture resizeTexture(int newWidth, int newHeight, String path) {
-        Pixmap pixmapOld = new Pixmap(Gdx.files.internal(path));
+    public Texture resizeTexture(int newWidth, int newHeight, Texture texture) {
+        TextureData textureData = texture.getTextureData();
+        textureData.prepare();
+        Pixmap pixmapOld = textureData.consumePixmap();
         Pixmap pixmapNew = new Pixmap(newWidth, newHeight, pixmapOld.getFormat());
         pixmapNew.drawPixmap(pixmapOld,
                 0, 0, pixmapOld.getWidth(), pixmapOld.getHeight(),
