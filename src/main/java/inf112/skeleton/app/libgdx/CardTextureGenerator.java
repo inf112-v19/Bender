2f7fb3b6015fd7bf4769b3bdde2c99d201671f72
Dovydas Sprindys <spdoviss@gmail.com>
2019-03-28 18:03:02 +0100

    Bug fixes

--- src/main/java/inf112/skeleton/app/libgdx/CardTextureGenerator.java
+++ src/main/java/inf112/skeleton/app/libgdx/CardTextureGenerator.java
@@ -3,14 +3,12 @@ package inf112.skeleton.app.libgdx;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import inf112.skeleton.app.core.cards.IProgramCard;
 import inf112.skeleton.app.core.cards.MoveCard;
 import inf112.skeleton.app.core.cards.RotateCard;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.BitmapFontCache;
 import inf112.skeleton.app.libgdx.utils.SpriteLoader;
 
 public class CardTextureGenerator {
@@ -25,7 +23,6 @@ public class CardTextureGenerator {
         straightArrow = new Texture(Gdx.files.internal("cards/arrow_forward.png"));
         rotateLeft = new Texture(Gdx.files.internal("cards/arrow_rotate_90_left.png"));
         basicCard = new Texture(Gdx.files.internal("cards/card.png"));
-
         spriteLoader = new SpriteLoader();
     }
 
@@ -33,7 +30,6 @@ public class CardTextureGenerator {
         if (card instanceof MoveCard)
             return generateMoveCard((MoveCard) card);
         return generateRotateCard((RotateCard) card);
-
     }
 
     private Texture generateRotateCard(RotateCard card) {
@@ -49,30 +45,28 @@ public class CardTextureGenerator {
     }
 
     private Texture makeUTurn() {
-        Texture rotated = textureEditor.rotate90(textureEditor.rotate90(rotateLeft));
-        return textureEditor.mergeTextures(rotated, basicCard, 20, 100, 140, 140);
+        return textureEditor.mergeTextures(textureEditor.rotate180(rotateLeft), basicCard, 20, 94, 140, 140);
     }
 
     private Texture makeLeftTurn() {
-        return textureEditor.mergeTextures(rotateLeft, basicCard, 20, 100, 140, 140);
+        return textureEditor.mergeTextures(rotateLeft, basicCard, 20, 94, 140, 140);
     }
 
     private Texture makeRightTurn() {
-        Texture flipped = textureEditor.flip(rotateLeft, false, true);
-        return textureEditor.mergeTextures(flipped, basicCard, 20, 100, 140, 140);
+        Texture flipped = textureEditor.rotate270(rotateLeft);
+        return textureEditor.mergeTextures(flipped, basicCard, 20, 94, 140, 140);
     }
 
     private Texture generateMoveCard(MoveCard card) {
         if (!card.movesBackwards()) {
             return generateForwardCard();
         }
-        Texture flipped = textureEditor.rotate90(textureEditor.rotate90(straightArrow));
-        return textureEditor.mergeTextures(flipped, basicCard, 20, 100, 140, 140);
-
+        Texture flipped = textureEditor.rotate180(straightArrow);
+        return textureEditor.mergeTextures(flipped, basicCard, 20, 94, 140, 140);
     }
 
     private Texture generateForwardCard() {
-        return textureEditor.mergeTextures(straightArrow, basicCard, 20, 100, 140, 140);
+        return textureEditor.mergeTextures(straightArrow, basicCard, 20, 94, 140, 140);
     }
 
     //Draws the amount a card is supposed to move the player forwards.
@@ -89,9 +83,6 @@ public class CardTextureGenerator {
         stage.getBatch().begin();
         bc.draw(stage.getBatch());
         stage.getBatch().end();
-    }
-
-    public void dispose() {
-
+        bc.clear();
     }
 }
