43012649e233549c91054222da4a4be78a2e46e7
RuneThe <rth002@uib.no>
2019-04-25 17:37:30 +0200

    small corrections

--- src/main/java/inf112/skeleton/app/core/lazer/Lazer.java
+++ src/main/java/inf112/skeleton/app/core/lazer/Lazer.java
@@ -6,19 +6,19 @@ import inf112.skeleton.app.core.board.events.LaserEvent;
 import inf112.skeleton.app.core.enums.Direction;
 import inf112.skeleton.app.core.position.Position;
 import inf112.skeleton.app.core.robot.Robot;
+import inf112.skeleton.app.core.tiles.ITile;
 import inf112.skeleton.app.core.tiles.Tile;
 
 public class Lazer {
 
     public Event shootLazer(Robot robot, Board board) {
-        LaserEvent lazerShoot;
         Position checkPosition = new Position(0,0);
-        // Can position be found from Robot or board?
         Direction dir = robot.getDirection();
         Position startPosition = board.getRobotPosition(robot);
         int PosX = startPosition.getX();
         int PosY = startPosition.getY();
-        // Go from position in direction of dir
+
+        // Go from position in direction of dir and look for Robot or wall
         switch (dir){
             // NORTH = y-- down to 0
             case NORTH:
@@ -29,8 +29,8 @@ public class Lazer {
                     board.getRobot(checkPosition).takeEnergy(1);
                 }
                 else if (true) {
-                    // Find a wall, and return
-                    Tile tile = board.getTile(checkPosition)
+                    // If wall is found return position to stop laser
+                    ITile tile = board.getTile(checkPosition);
                     // if (tile.hasWall) {}
                 }
             }
@@ -44,7 +44,7 @@ public class Lazer {
                 }
                 else if (true) {
                     // Find a wall, and return
-                    Tile tile = board.getTile(checkPosition)
+                    ITile tile = board.getTile(checkPosition);
                     // if (tile.hasWall) {}
                 }
             }
@@ -58,7 +58,7 @@ public class Lazer {
                     }
                     else if (true) {
                         // Find a wall, and return
-                        Tile tile = board.getTile(checkPosition)
+                        ITile tile = board.getTile(checkPosition);
                         // if (tile.hasWall) {}
                     }
             }
@@ -72,7 +72,7 @@ public class Lazer {
                     }
                     else if (true) {
                         // Find a wall, and return
-                        Tile tile = board.getTile(checkPosition)
+                        ITile tile = board.getTile(checkPosition);
                         // if (tile.hasWall) {}
                     }
             }
