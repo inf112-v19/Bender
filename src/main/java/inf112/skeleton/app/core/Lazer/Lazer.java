1f055c14629f1c226f5e14f9b606818b56cb59cd
RuneThe <rth002@uib.no>
2019-04-25 13:43:34 +0200

    more work on lazer

--- src/main/java/inf112/skeleton/app/core/Lazer/Lazer.java
+++ src/main/java/inf112/skeleton/app/core/Lazer/Lazer.java
@@ -1,26 +1,41 @@
 package inf112.skeleton.app.core.Lazer;
 
+import inf112.skeleton.app.core.board.Board;
+import inf112.skeleton.app.core.board.events.Event;
+import inf112.skeleton.app.core.board.events.LaserEvent;
 import inf112.skeleton.app.core.enums.Direction;
 import inf112.skeleton.app.core.position.Position;
 import inf112.skeleton.app.core.robot.Robot;
 
 public class Lazer {
 
-    public void shootLazer(Robot robot) {
+    public Event shootLazer(Robot robot, Board board) {
+        LaserEvent lazerShoot;
         // Can position be found from Robot or board?
         Direction dir = robot.getDirection();
+        int PosX = board.getRobotPosition(robot).getX();
+        int posY = board.getRobotPosition(robot).getY();
         // Go from position in direction of dir
-        // NORTH = y-- until 0
-        // SOUTH = y++ until 11
-        // EAST = x++ until 11
-        // WEST = x-- until 0
+        switch (dir){
+            // NORTH = y-- until 0
+            case NORTH:
+            {for (int i=PosX; i< )}
+            // SOUTH = y++ until 11
+            case SOUTH:
+            {}
+            // SOUTH = y++ until 11
+            case EAST:
+            {}
+            // WEST = x-- until 0
+            case WEST:
+            {}
+
+        }
         // Iterate each tile, and check for wall or robot
         // If found wall, only stop lazer
         // If found robot initiate damage to robot, and then stop lazer
 
+
+        return lazerShoot;
     }
 }
-// Shooting lazer needs from position and to position
-// And to check if it hits another robot
-// And initiate damage on this robot, and stop lazer
-// Or only stop lazer if it hits a wall
